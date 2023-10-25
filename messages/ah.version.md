ec9c79abfeebeb7457e86ae13429d13fa6d673b5
Juan José Longoria <juanjoselongoria@gmail.com>
2023-10-26 00:21:01 +0200

    feat(Commands): converted version command
    Converted Version Command from SFDX Format to SF Format

---
+++ messages/ah.version.md
@@ -0,0 +1,17 @@
+# summary
+
+Get Installed Aura Helper SF Version.
+
+# description
+
+Command for get the installed Aura Helper SF version.
+
+# examples
+
+- Get the installed Aura Helper SF version.
+
+  <%= config.bin %> <%= command.id %>
+
+- Get the installed Aura Helper SF version in JSON format.
+
+  <%= config.bin %> <%= command.id %> --json
---
+++ src/commands/ah/version.ts
@@ -0,0 +1,35 @@
+import { SfCommand } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+import { FileReader, PathUtils } from '@aurahelper/core';
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.loadMessages('aura-helper-sf', 'ah.version');
+
+export interface PackageFile {
+  version: string;
+}
+
+export type AhVersionResult = {
+  version: string;
+};
+
+export default class AhVersion extends SfCommand<AhVersionResult> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+
+  public async run(): Promise<AhVersionResult> {
+    return new Promise((resolve) => {
+      let dirName: string = __dirname;
+      dirName = PathUtils.getDirname(dirName);
+      dirName = PathUtils.getDirname(dirName);
+      dirName = PathUtils.getDirname(dirName);
+      dirName = PathUtils.getAbsolutePath(dirName);
+      const packageDir = dirName + '/package.json';
+      const content: string = FileReader.readFileSync(packageDir);
+      const config = JSON.parse(content) as PackageFile;
+      this.log(`Aura Helper SFDX Version: v${config.version}`);
+      resolve({ version: config.version });
+    });
+  }
+}
